4999c555108778fb1ff03cc0934a47f045b3791b
Rishita Shaw <rishitashaw49@gmail.com>
2021-10-18 19:11:18 +0530

    updated version

--- src/script.js
+++ src/script.js
@@ -5,12 +5,12 @@ import vertex from "./shaders/vertex.glsl";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export default class Sketch {
-  constructor() {
-    this.renderer = new THREE.WebGLRenderer({
+  constructor () {
+    this.renderer = new THREE.WebGLRenderer( {
       antialias: true,
-    });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById("container").appendChild(this.renderer.domElement);
+    } );
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    document.getElementById( "container" ).appendChild( this.renderer.domElement );
 
     this.camera = new THREE.PerspectiveCamera(
       70,
@@ -18,44 +18,33 @@ export default class Sketch {
       0.1,
       3000
     );
-    this.camera.position.z = 1000;
+    this.camera.position.z = 2;
 
     this.scene = new THREE.Scene();
     this.time = 0;
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls = new OrbitControls( this.camera, this.renderer.domElement );
     this.addMesh();
 
     this.render();
   }
 
   addMesh() {
-    this.material = new THREE.ShaderMaterial({
+    this.material = new THREE.ShaderMaterial( {
       fragmentShader: fragment,
       vertexShader: vertex,
       uniforms: {
-        progress: { type: "f", value: 0 },
+        time: { value: 0 },
+        resolution: { value: new THREE.Vector4() }
       },
       side: THREE.DoubleSide,
-    });
+    } );
 
-    let number = 512 * 512;
+    this.geometry = new THREE.PlaneGeometry( 1, 1, 1, 1 )
 
-    this.geometry = new THREE.BufferGeometry();
-    this.position = new THREE.BufferAttribute(new Float32Array(number * 3), 3);
 
-    let index = 0;
-    for (let i = 0; i < 512; i++) {
-      let posX = i - 256;
-      for (let j = 0; j < 512; j++) {
-        this.position.setXYZ(index, posX * 2, (j - 256) * 2, 0);
-        index++;
-      }
-    }
 
-    this.geometry.setAttribute("position", this.position);
-
-    this.mesh = new THREE.Points(this.geometry, this.material);
-    this.scene.add(this.mesh);
+    this.mesh = new THREE.Points( this.geometry, this.material );
+    this.scene.add( this.mesh );
   }
 
   render() {
@@ -63,8 +52,8 @@ export default class Sketch {
     //this.mesh.rotation.x += 0.01;
     //this.mesh.rotation.y += 0.01;
 
-    this.renderer.render(this.scene, this.camera);
-    window.requestAnimationFrame(this.render.bind(this));
+    this.renderer.render( this.scene, this.camera );
+    window.requestAnimationFrame( this.render.bind( this ) );
   }
 }
 
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,10 +1,12 @@
+uniform float time;
 varying vec2 vUv;
+varying vec3 vPosition;
+uniform sampler2D texture1;
+float PI = 3.14159265358979323846;
 
-void main() {
+void main(){
     vUv=uv;
-
-    vec4 mvPosition = modelViewMatrix * vec4 (position, 1.);
-
-    gl_PointSize = 2000. *(1. / -mvPosition.z);
-    gl_Position=projectionMatrix * mvPosition;
+    vec4 mvPosition = modelViewMatrix*vec4(position, 1.);
+    gl_PointSize=100. * (1./-mvPosition.z);
+    gl_Position=projectionMatrix*mvPosition;
 }
